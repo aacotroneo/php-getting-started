1379d2de487ba432a2fe049aabea21dba7b26090
unknown <aacotroneo@gmail.com>
2015-01-23 00:21:24 -0300

    deps

--- web/index.php
+++ web/index.php
@@ -2,21 +2,7 @@
 
 require('../vendor/autoload.php');
 
-$app = new Silex\Application();
-$app['debug'] = true;
+$app = new \Aac\App();
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
-});
-
-$app->run();
 
 ?>
